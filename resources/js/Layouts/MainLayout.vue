d0a04a4f56eff44d576ac7150aff28e44ed280f6
Jamickin <jamickina@gmail.com>
2024-05-25 13:27:43 +0200

    Update

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -1,14 +1,14 @@
 <template>
     <header
-        class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-800 w-full"
+        class="border-b border-gray-200 dark:border-gray-700 bg-white dark:bg-gray-900 w-full"
     >
         <div class="container mx-auto">
             <nav class="p-4 flex items-center justify-between">
                 <div class="text-lg font-medium">
-                    <Link :href="route('listing.index')">Listings</Link>&nbsp;
+                    <Link :href="route('listing.index')">Listings</Link>
                 </div>
                 <div
-                    class="text-xl text-indigo-700 dark:text-indigo-300 font-bold text-center"
+                    class="text-xl text-indigo-600 dark:text-indigo-300 font-bold text-center"
                 >
                     <Link :href="route('listing.index')">LaraZillow</Link>
                 </div>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -1,6 +1,9 @@
 <template>
     <div>Show</div>
     <div>The new message is {{ message }}</div>
+    <Price :price="listing.price" class="text-2xl font-bold" />
+    <ListingSpace :listing="listing" class="text-lg" />
+    <ListingAddress :listing="listing" class="text-gray-500" />
 </template>
 
 <script setup>
